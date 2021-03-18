94a3a41007a78939c7da1fc7d4a8bde563baa29d
John Moehrke <JohnMoehrke@gmail.com>
2021-03-18 12:42:25 -0500

    update to formatcode for version stamp. History still not working fully

---
+++ fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide/ihe.formatcode.fhir/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.html');
+else 
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/0.2.2/ImplementationGuide-ihe.formatcode.fhir.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
