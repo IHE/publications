828fb13f26165cfdae01dd83f8f7804c53a3655b
John Moehrke <JohnMoehrke@gmail.com>
2021-06-03 15:13:22 -0500

    Release FormatCode 1.0.0

---
+++ fhir/ihe.formatcode.fhir/1.0.0/Bundle/history-IHE-formatcode.valueset/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.html');
+else 
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/Bundle-history-IHE-formatcode.valueset.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ fhir/ihe.formatcode.fhir/1.0.0/ValueSet/formatcode/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.html');
+else 
+  Redirect('http://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.0.0/ValueSet-formatcode.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
