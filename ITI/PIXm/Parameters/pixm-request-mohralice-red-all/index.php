00406aca838137c3a569ad841fbb92d6ccaef3fa
John Moehrke <JohnMoehrke@gmail.com>
2022-02-28 19:21:33 -0600

    rebuild PIXm

---
+++ ITI/PIXm/Parameters/pixm-request-mohralice-red-all/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Parameters-pixm-request-mohralice-red-all.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ ITI/PIXm/Patient/Patient-MohrAlissa-Red/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/PIXm/3.0.2/Patient-Patient-MohrAlissa-Red.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
