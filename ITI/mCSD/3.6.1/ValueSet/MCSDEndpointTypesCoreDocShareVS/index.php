9fd5dfc2ab2129cb590ff9f6c93562beac81dd3d
John Moehrke <JohnMoehrke@gmail.com>
2022-03-02 08:33:09 -0600

    mCSD 3.6.1

---
+++ ITI/mCSD/3.6.1/ValueSet/MCSDEndpointTypesCoreDocShareVS/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDEndpointTypesCoreDocShareVS.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ ITI/mCSD/3.6.1/ValueSet/MCSDOrgAffTypesVS/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/mCSD/3.6.1/ValueSet-MCSDOrgAffTypesVS.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
