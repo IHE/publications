818c5cf75c73112c8762e36fce8db2bf8818b039
JohnMoehrke <JohnMoehrke@gmail.com>
2023-11-16 15:06:48 -0600

    release PDQm-match public-comment

---
+++ ITI/PDQm/3.0.0-comment/OperationOutcome/ex-match-failure/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/PDQm/3.0.0-comment/OperationOutcome-ex-match-failure.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
