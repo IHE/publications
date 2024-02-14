4b73fd1943fb9271629e4dbe07bec9779457af1a
JohnMoehrke <JohnMoehrke@gmail.com>
2024-02-14 10:52:19 -0600

    Release ITI.BALP 1.1.3

---
+++ ITI/BALP/1.1.3/ValueSet/AllUpdateVS/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.html');
+else 
+  Redirect('https://profiles.ihe.net/ITI/BALP/1.1.3/ValueSet-AllUpdateVS.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
