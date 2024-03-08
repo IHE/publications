bc63402254da45e7f533fe8b8a787df118741a3d
JohnMoehrke <JohnMoehrke@gmail.com>
2024-03-08 10:49:10 -0600

    Release PCC.ODH

---
+++ PCC/ODH/1.0.0-comment/StructureDefinition/odh-EmploymentStatus/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.html');
+else 
+  Redirect('https://profiles.ihe.net/PCC/ODH/1.0.0-comment/StructureDefinition-odh-EmploymentStatus.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
