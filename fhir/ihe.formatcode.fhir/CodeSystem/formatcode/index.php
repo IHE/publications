c6a84f32ac9aac2d0a3e4b86bc577576110852ff
Mary Jungers <mjungers@ihe.net>
2026-03-14 21:07:26 -0500

    updates for FormatCode 1.5.0 release

--- fhir/ihe.formatcode.fhir/CodeSystem/formatcode/index.php
+++ fhir/ihe.formatcode.fhir/CodeSystem/formatcode/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.json2');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.json1');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.json');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.xml2');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.xml1');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.html');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.html');
 else 
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/CodeSystem-formatcode.xml');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/CodeSystem-formatcode.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
--- fhir/ihe.formatcode.fhir/ImplementationGuide/ihe.formatcode.fhir/index.php
+++ fhir/ihe.formatcode.fhir/ImplementationGuide/ihe.formatcode.fhir/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.json2');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.json1');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.json');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.xml2');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.xml1');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/index.html');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/index.html');
 else 
-  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.4.0/ImplementationGuide-ihe.formatcode.fhir.xml');
+  Redirect('https://profiles.ihe.net/fhir/ihe.formatcode.fhir/1.5.0/ImplementationGuide-ihe.formatcode.fhir.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
